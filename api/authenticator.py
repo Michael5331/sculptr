a6cf0976d6bda6539fc01ff03300d4f86d73b6e1
Michael5331 <sgtparn@gmail.com>
2023-11-01 14:31:01 -0400

    Authenticator added

---
+++ api/authenticator.py
@@ -0,0 +1,29 @@
+import os
+from fastapi import Depends
+from jwtdown_fastapi.authentication import Authenticator
+from queries.accounts import AccountQueries, AccountOut, AccountOutWithPassword
+
+
+class MyAuthenticator(Authenticator):
+    async def get_account_data(
+            self, 
+            email: str, 
+            accounts: AccountQueries, 
+    ):
+        return accounts.get(email)
+    
+    def get_account_getter(
+            self, 
+            accounts: AccountQueries = Depends(),n  
+    ):
+        return accounts
+    
+    def get_hashed_password(self, account: AccountOutWithPassword):
+        return account.password
+    
+    def get_account_data_for_cookie(self, account: AccountOutWithPassword):
+        return account.email, AccountOut(**account.dict())
+    
+
+authenticator = MyAuthenticator(os.environ["SIGNING_KEY"])
+    
